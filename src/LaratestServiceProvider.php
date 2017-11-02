ec963c3523ac5dccda8482115a51e97fbb082522
Brian Lasher <me@brianlasher.com>
2017-11-02 16:54:53 -0500

    initial attempt at service provider for vendor:publsh

---
+++ src/LaratestServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaratestServiceProvider extends ServiceProvider
+{
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->publishes([
+            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('ApiAuthenticatable.php'),
+            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('ApiAuthTestInterface.php'),
+            __DIR__.'/tests/API/ApiTokenAuthTest.php'     => $this->test_path('ApiTokenAuthTest.php.php')
+        ], 'token');
+
+        $this->publishes([
+            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('ApiAuthenticatable.php'),
+            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('ApiAuthTestInterface.php'),
+            __DIR__.'/tests/API/ApiPassportAuthTest.php'  => $this->test_path('ApiPassportAuthTest.php')
+        ], 'passport');
+
+
+        parent::boot();
+    }
+
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/tests' . ($path ? '/' . $path : $path);
+    }
+    
+}
