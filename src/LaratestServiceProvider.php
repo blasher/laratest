128b2ced436d03e509f90b884066ba374b5426d7
Brian Lasher <me@brianlasher.com>
2017-11-02 21:28:51 -0500

    path corrections

--- src/LaratestServiceProvider.php
+++ src/LaratestServiceProvider.php
@@ -13,21 +13,29 @@ class LaratestServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
         $this->publishes([
-            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('ApiAuthenticatable.php'),
-            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('ApiAuthTestInterface.php'),
-            __DIR__.'/tests/API/ApiTokenAuthTest.php'     => $this->test_path('ApiTokenAuthTest.php.php')
+            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('API/ApiAuthenticatable.php'),
+            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('API/ApiAuthTestInterface.php'),
+            __DIR__.'/tests/API/ApiTokenAuthTest.php'     => $this->test_path('API/ApiTokenAuthTest.php.php')
         ], 'laratest_token');
 
         $this->publishes([
-            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('ApiAuthenticatable.php'),
-            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('ApiAuthTestInterface.php'),
-            __DIR__.'/tests/API/ApiPassportAuthTest.php'  => $this->test_path('ApiPassportAuthTest.php')
+            __DIR__.'/tests/API/ApiAuthenticatable.php'   => $this->test_path('API/ApiAuthenticatable.php'),
+            __DIR__.'/tests/API/ApiAuthTestInterface.php' => $this->test_path('API/ApiAuthTestInterface.php'),
+            __DIR__.'/tests/API/ApiPassportAuthTest.php'  => $this->test_path('API/ApiPassportAuthTest.php')
         ], 'laratest_passport');
 
 
         parent::boot();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
     }
 
     /**
